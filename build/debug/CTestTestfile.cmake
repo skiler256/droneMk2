176a8fd6d456a9a431fee27b08b8215c7fc75b7c
skiler256 <julesmiceli@gmail.com>
2026-05-13 18:49:03 +0000

    first

---
+++ build/debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/droneMk2
+# Build directory: /workspaces/droneMk2/build/debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[test_types]=] "/workspaces/droneMk2/build/debug/test_types")
+set_tests_properties([=[test_types]=] PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/droneMk2/CMakeLists.txt;112;add_test;/workspaces/droneMk2/CMakeLists.txt;116;drone_test;/workspaces/droneMk2/CMakeLists.txt;0;")
+add_test([=[test_shared_state]=] "/workspaces/droneMk2/build/debug/test_shared_state")
+set_tests_properties([=[test_shared_state]=] PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/droneMk2/CMakeLists.txt;112;add_test;/workspaces/droneMk2/CMakeLists.txt;117;drone_test;/workspaces/droneMk2/CMakeLists.txt;0;")
